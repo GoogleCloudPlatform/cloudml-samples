2f8e72098655b1e658052a881ef1b14307eb8555
elibixby <elibixby@google.com>
2017-02-26 18:26:38 -0800

    Significant reorganization. Still Occasional Errors. WIP

--- census/vanilla-tf/trainer/model.py
+++ census/vanilla-tf/trainer/model.py
@@ -20,24 +20,8 @@
 
 import tensorflow as tf
 
-def random_normal():
-  """Random normal initializer."""
-  return tf.random_normal_initializer(mean=0.0, stddev=0.1)
 
-def add_layer(curr_layer, layer_name, weight_name, bias_name, shape, ops):
-  """Create and add a layer in the neural network."""
-  with tf.variable_scope(layer_name):
-    weight = tf.get_variable(weight_name,
-                             shape,
-                             initializer=random_normal())
-
-    bias = tf.get_variable(bias_name,
-                           shape[1],
-                           initializer=tf.zeros_initializer(tf.float32))
-
-    return apply(ops, [tf.matmul(curr_layer, weight) + bias])
-
-def inference(input_x, hidden_units=[100,70,50,25], num_classes=2):
+def dnn(inputs, hidden_units=[100,70,50,25], num_classes=2):
   """Create a Feed forward network running on single node
 
   Args:
@@ -46,22 +30,27 @@ def inference(input_x, hidden_units=[100,70,50,25], num_classes=2):
     num_classes (int): Number of classes
 
   Returns:
-    Feed forward NN with relu and softmax layer
+    The last layer values
   """
-  layers_size = [input_x.get_shape()[1]] + hidden_units
-  layers_shape = zip(layers_size[0:],layers_size[1:])
+  print(inputs.get_shape())
+  layers_size = [inputs.get_shape()[1]] + hidden_units
+  layers_shape = zip(layers_size[0:],layers_size[1:] + [num_classes])
 
-  curr_layer = input_x
+  curr_layer = inputs
+  with tf.variable_scope('dnn',
+                         initializer=tf.truncated_normal_initializer()):
+    # Creates the relu hidden layers
+    for num, shape in enumerate(layers_shape):
+      with tf.variable_scope('relu_{}'.format(num)):
 
-  # Creates the relu hidden layers
-  for num, shape in enumerate(layers_shape):
-    curr_layer = add_layer(curr_layer, 'relu_{}'.format(num),
-                           'relu_w_{}'.format(num), 'relu_b_{}'.format(num),
-                           shape, tf.nn.relu)
+        weights = tf.get_variable('weights', shape)
+
+        biases = tf.get_variable(
+            'biases', shape[1], initializer=tf.zeros_initializer(tf.float32))
+
+      curr_layer = tf.nn.relu(tf.matmul(curr_layer, weights) + biases)
+
+    return curr_layer
 
 
-  # Creates the softmax output layer
-  shape = [hidden_units[-1], num_classes]
-  curr_layer = add_layer(curr_layer, 'softmax', 'softmax_w', 'softmax_b',
-                         shape, tf.nn.softmax)
-  return curr_layer
+
