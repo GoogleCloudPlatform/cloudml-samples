cfcd40bcd77e369298626060d08fbcb421aa5524
Eli Bixby <elibixby@google.com>
2017-07-17 20:35:33 -0700

    Fix python installation and if statement

--- testing/ubuntu/presubmit/run_if_changed.sh
+++ testing/ubuntu/presubmit/run_if_changed.sh
@@ -4,7 +4,7 @@ set -e
 ### Ignore this test if there are no relevant changes
 cd ${CMLE_REPO_DIR}/${CMLE_TEST_BASE_DIR}
 
-if [[-z `git diff $KOKORO_GITHUB_PULL_REQUEST_COMMIT ${KOKORO_GITHUB_PULL_REQUEST_COMMIT}~1 $PWD`]]
+if [ -z `git diff $KOKORO_GITHUB_PULL_REQUEST_COMMIT ${KOKORO_GITHUB_PULL_REQUEST_COMMIT}~1 $PWD` ]
 then
     echo "TEST IGNORED; directory not modified in pull request $KOKORO_GITHUB_PULL_REQUEST_NUMBER"
     exit 0
@@ -15,6 +15,16 @@ fi
 
 sudo apt-get install python-dev python-pip
 
+curl -L https://raw.githubusercontent.com/pyenv/pyenv-installer/master/bin/pyenv-installer | bash
+
+sudo pip install virtualenv
+virtualenv ${KOKORO_ARTIFACTS_DIR}/envs/tf
+source ${KOKORO_ARTIFACTS_DIR}/envs/tf/bin/activate
+
+# Ubuntu stock distribution of pip is out of date
+pip install --upgrade pip
+
+# Install test requirements
 pip install --upgrade -r $CMLE_REQUIREMENTS_FILE
 
 gcloud auth activate-service-account --key-file "${KOKORO_GFILE_DIR}/${CMLE_KEYFILE}"
