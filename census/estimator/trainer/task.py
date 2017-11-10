68eb521210670ab655d9ae08c260007fac12ed18
Puneith Kaul <puneith@google.com>
2017-11-10 15:45:43 -0800

    added experiment function

--- census/estimator/trainer/task.py
+++ census/estimator/trainer/task.py
@@ -4,59 +4,52 @@ import os
 import model
 
 import tensorflow as tf
-from tensorflow.contrib.learn.python.learn import learn_runner
-from tensorflow.contrib.learn.python.learn.estimators import run_config
 from tensorflow.contrib.learn.python.learn.utils import (
     saved_model_export_utils)
 from tensorflow.contrib.training.python.training import hparam
 
 
-def generate_experiment_fn(**experiment_args):
-  """Create an experiment function.
+def run_experiment(hparams):
+  """Run the training and evaluate using the high level API"""
 
-  See command line help text for description of args.
-  Args:
-    experiment_args: keyword arguments to be passed through to experiment
-      See `tf.contrib.learn.Experiment` for full args.
-  Returns:
-    A function:
-      (tf.contrib.learn.RunConfig, tf.contrib.training.HParams) -> Experiment
+  train_input = lambda: model.generate_input_fn(
+      hparams.train_files,
+      num_epochs=hparams.num_epochs,
+      batch_size=hparams.train_batch_size
+  )
 
-    This function is used by learn_runner to create an Experiment which
-    executes model code provided in the form of an Estimator and
-    input functions.
-  """
-  def _experiment_fn(run_config, hparams):
-    # num_epochs can control duration if train_steps isn't
-    # passed to Experiment
-    train_input = lambda: model.generate_input_fn(
-        hparams.train_files,
-        num_epochs=hparams.num_epochs,
-        batch_size=hparams.train_batch_size,
-    )
-    # Don't shuffle evaluation data
-    eval_input = lambda: model.generate_input_fn(
-        hparams.eval_files,
-        batch_size=hparams.eval_batch_size,
-        shuffle=False
-    )
-    return tf.contrib.learn.Experiment(
-        model.build_estimator(
-            embedding_size=hparams.embedding_size,
-            # Construct layers sizes with exponetial decay
-            hidden_units=[
-                max(2, int(hparams.first_layer_size *
-                           hparams.scale_factor**i))
-                for i in range(hparams.num_layers)
-            ],
-            config=run_config
-        ),
-        train_input_fn=train_input,
-        eval_input_fn=eval_input,
-        **experiment_args
-    )
-  return _experiment_fn
+  # Don't shuffle evaluation data
+  eval_input = lambda: model.generate_input_fn(
+      hparams.eval_files,
+      batch_size=hparams.eval_batch_size,
+      shuffle=False
+  )
 
+  train_spec = tf.estimator.TrainSpec(train_input,
+                                      max_steps=hparams.train_steps
+                                      )
+
+  eval_spec = tf.estimator.EvalSpec(eval_input,
+                                    steps=hparams.eval_steps,
+                                    name='census-eval'
+                                    )
+
+  run_config = tf.estimator.RunConfig()
+  run_config.replace(model_dir=hparams.job_dir)
+  estimator = model.build_estimator(
+      embedding_size=hparams.embedding_size,
+      # Construct layers sizes with exponetial decay
+      hidden_units=[
+          max(2, int(hparams.first_layer_size *
+                     hparams.scale_factor**i))
+          for i in range(hparams.num_layers)
+      ],
+      config=run_config
+  )
+
+  tf.estimator.train_and_evaluate(estimator,
+                                  train_spec,
+                                  eval_spec)
 
 if __name__ == '__main__':
   parser = argparse.ArgumentParser()
@@ -200,46 +193,5 @@ if __name__ == '__main__':
     tf.logging.info("Reusing job_dir {} if it exists".format(args.job_dir))
 
   # Run the training job
-  # learn_runner pulls configuration information from environment
-  # variables using tf.learn.RunConfig and uses this configuration
-  # to conditionally execute Experiment, or param server code
-
   hparams=hparam.HParams(**args.__dict__)
-  train_input = lambda: model.generate_input_fn(
-      hparams.train_files,
-      num_epochs=hparams.num_epochs,
-      batch_size=hparams.train_batch_size
-  )
-
-    # Don't shuffle evaluation data
-  eval_input = lambda: model.generate_input_fn(
-      hparams.eval_files,
-      batch_size=hparams.eval_batch_size,
-      shuffle=False
-  )
-
-  train_spec = tf.estimator.TrainSpec(train_input,
-                                      max_steps=args.train_steps
-                                      )
-
-  eval_spec = tf.estimator.EvalSpec(eval_input,
-                                    steps=args.eval_steps,
-                                    name='census-eval'
-                                    )
-
-  run_config = tf.estimator.RunConfig()
-  run_config.replace(model_dir=args.job_dir)
-  estimator = model.build_estimator(
-      embedding_size=hparams.embedding_size,
-      # Construct layers sizes with exponetial decay
-      hidden_units=[
-          max(2, int(hparams.first_layer_size *
-                     hparams.scale_factor**i))
-          for i in range(hparams.num_layers)
-      ],
-      config=run_config
-  )
-
-  tf.estimator.train_and_evaluate(estimator,
-                                  train_spec,
-                                  eval_spec)
+  run_experiment(hparams)
