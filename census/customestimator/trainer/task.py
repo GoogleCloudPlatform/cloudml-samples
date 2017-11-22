83b6dc88900dd4ef44507c26e061a711913ec7b2
Puneith Kaul <puneith@google.com>
2017-11-21 18:18:26 -0800

    modified custom estimator as per TF1.4 API changes

--- census/customestimator/trainer/task.py
+++ census/customestimator/trainer/task.py
@@ -10,38 +10,35 @@ from tensorflow.contrib.learn.python.learn.utils import (
 from tensorflow.contrib.training.python.training import hparam
 
 
-def generate_experiment_fn(**experiment_args):
-  """Create an experiment function.
+def run_experiment(hparams):
+  """Run the training and evaluate using the high level API"""
 
-  See command line help text for description of args.
-  Args:
-    experiment_args: keyword arguments to be passed through to experiment
-      See `tf.contrib.learn.Experiment` for full args.
-  Returns:
-    A function:
-      (tf.contrib.learn.RunConfig, tf.contrib.training.HParams) -> Experiment
+  train_input = lambda: model.generate_input_fn(
+      hparams.train_files,
+      num_epochs=hparams.num_epochs,
+      batch_size=hparams.train_batch_size
+  )
 
-    This function is used by learn_runner to create an Experiment which
-    executes model code provided in the form of an Estimator and
-    input functions.
-  """
-  def _experiment_fn(run_config, hparams):
-    # num_epochs can control duration if train_steps isn't
-    # passed to Experiment
-    train_input = lambda: model.generate_input_fn(
-        hparams.train_files,
-        num_epochs=hparams.num_epochs,
-        batch_size=hparams.train_batch_size,
-    )
-    # Don't shuffle evaluation data
-    eval_input = lambda: model.generate_input_fn(
-        hparams.eval_files,
-        batch_size=hparams.eval_batch_size,
-        shuffle=False
-    )
-    return tf.contrib.learn.Experiment(
-        tf.estimator.Estimator(
-            model.generate_model_fn(
+  # Don't shuffle evaluation data
+  eval_input = lambda: model.generate_input_fn(
+      hparams.eval_files,
+      batch_size=hparams.eval_batch_size,
+      shuffle=False
+  )
+
+  train_spec = tf.estimator.TrainSpec(train_input,
+                                      max_steps=hparams.train_steps
+                                      )
+
+  exporter = tf.estimator.FinalExporter('census',
+          model.SERVING_FUNCTIONS[hparams.export_format])
+  eval_spec = tf.estimator.EvalSpec(eval_input,
+                                    steps=hparams.eval_steps,
+                                    exporters=[exporter],
+                                    name='census-eval'
+                                    )
+
+  model_fn = model.generate_model_fn(
                 embedding_size=hparams.embedding_size,
                 # Construct layers sizes with exponetial decay
                 hidden_units=[
@@ -49,16 +46,12 @@ def generate_experiment_fn(**experiment_args):
                                hparams.scale_factor**i))
                     for i in range(hparams.num_layers)
                 ],
-                learning_rate=hparams.learning_rate
-            ),
-            config=run_config
-        ),
-        train_input_fn=train_input,
-        eval_input_fn=eval_input,
-        **experiment_args
-    )
-  return _experiment_fn
+                learning_rate=hparams.learning_rate)
 
+  estimator = tf.estimator.Estimator(model_fn=model_fn,model_dir=hparams.job_dir)
+  tf.estimator.train_and_evaluate(estimator,
+                                  train_spec,
+                                  eval_spec)
 
 if __name__ == '__main__':
   parser = argparse.ArgumentParser()
@@ -134,16 +127,6 @@ if __name__ == '__main__':
       required=True
   )
   parser.add_argument(
-      '--reuse-job-dir',
-      action='store_true',
-      default=False,
-      help="""\
-          Flag to decide if the model checkpoint should
-          be re-used from the job-dir. If False then the
-          job-dir will be deleted
-          """
-  )
-  parser.add_argument(
       '--verbosity',
       choices=[
           'DEBUG',
@@ -200,32 +183,6 @@ if __name__ == '__main__':
   os.environ['TF_CPP_MIN_LOG_LEVEL'] = str(
       tf.logging.__dict__[args.verbosity] / 10)
 
-  # If job_dir_reuse is False then remove the job_dir if it exists
-  if not args.reuse_job_dir:
-    if tf.gfile.Exists(args.job_dir):
-      tf.gfile.DeleteRecursively(args.job_dir)
-      tf.logging.info("Deleted job_dir {} to avoid re-use".format(args.job_dir))
-    else:
-      tf.logging.info("No job_dir available to delete")
-  else:
-    tf.logging.info("Reusing job_dir {} if it exists".format(args.job_dir))
-
-
   # Run the training job
-  # learn_runner pulls configuration information from environment
-  # variables using tf.learn.RunConfig and uses this configuration
-  # to conditionally execute Experiment, or param server code
-  learn_runner.run(
-      generate_experiment_fn(
-          min_eval_frequency=args.min_eval_frequency,
-          eval_delay_secs=args.eval_delay_secs,
-          train_steps=args.train_steps,
-          eval_steps=args.eval_steps,
-          export_strategies=[saved_model_export_utils.make_export_strategy(
-              model.SERVING_FUNCTIONS[args.export_format],
-              exports_to_keep=1
-          )]
-      ),
-      run_config=tf.contrib.learn.RunConfig(model_dir=args.job_dir),
-      hparams=hparam.HParams(**args.__dict__)
-  )
+  hparams=hparam.HParams(**args.__dict__)
+  run_experiment(hparams)
