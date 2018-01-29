b241171c93b893bbe7c7b76857990b1dbfd4d825
Andrew Maguire <andrewm4894@gmail.com>
2018-01-29 13:04:33 +0000

    Update setup.py
    Proposed resolution here https://github.com/GoogleCloudPlatform/cloudml-samples/issues/146#issuecomment-360876931

--- reddit_tft/setup.py
+++ reddit_tft/setup.py
@@ -17,8 +17,9 @@ import setuptools
 NAME = 'trainer'
 VERSION = '1.0'
 TENSORFLOW_TRANSFORM = 'tensorflow-transform==0.1.10'
-
+PROTOBUF = 'protobuf==3.4.0'
+TENSORFLOW = 'tensorflow==1.3.0'
 
 if __name__ == '__main__':
   setuptools.setup(name=NAME, version=VERSION, packages=['trainer'],
-                   install_requires=[TENSORFLOW_TRANSFORM])
+                   install_requires=[TENSORFLOW_TRANSFORM,PROTOBUF,TENSORFLOW])
