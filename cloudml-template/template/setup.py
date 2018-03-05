c4bf47631b301441872eaaf78e26173339209d88
khalid-m-salama <khalid.m.salama@gmail.com>
2018-03-05 21:30:52 +0000

    Directory re-structure + Readme updates

---
+++ cloudml-template/template/setup.py
@@ -0,0 +1,13 @@
+from setuptools import find_packages
+from setuptools import setup
+
+REQUIRED_PACKAGES = ['tensorflow>=1.4']
+
+setup(
+    name='',
+    version='',
+    install_requires=REQUIRED_PACKAGES,
+    packages=find_packages(),
+    include_package_data=True,
+    description=''
+)
