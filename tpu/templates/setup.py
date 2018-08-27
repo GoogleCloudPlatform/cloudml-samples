a2a46dcbc1ff64d730bfcc74f09ac46949276005
Yu-Han Liu <yuhanliu@google.com>
2018-08-27 11:48:44 -0700

    add templates

---
+++ tpu/templates/setup.py
@@ -0,0 +1,7 @@
+import setuptools
+
+setuptools.setup(name='tpu-template',
+      version='0.0.1',
+      install_requires=[],
+      packages=setuptools.find_packages()
+)
---
+++ tpu/templates/tpu_keras/trainer.py
@@ -0,0 +1,83 @@
+# Copyright 2018 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+import argparse
+import os
+import numpy as np
+import tensorflow as tf
+from tensorflow.keras.layers import LSTM
+
+
+def build_model():
+    inputs = tf.keras.Input(shape=(5, 3))
+    encoded = tf.keras.layers.LSTM(10)(inputs)
+    outputs = tf.keras.layers.Dense(1, activation=tf.nn.sigmoid)(encoded)
+
+    model = tf.keras.Model(inputs=inputs, outputs=outputs)
+
+    return model
+
+
+def make_data():
+    data_size = 100
+    sequences = np.random.random((data_size, 5, 3))
+    labels = np.random.randint(0, 2, size=(data_size,))
+
+    return sequences, labels
+
+
+def main(args):
+    model = build_model()
+
+    if args.use_tpu:
+        # distribute over TPU cores
+        # Note: only in TensorFlow 1.10+
+        strategy = tf.contrib.tpu.TPUDistributionStrategy(num_cores_per_host=8)
+        model = tf.contrib.tpu.keras_to_tpu_model(
+            model, strategy=strategy, tpu_name_or_address=args.tpu)
+
+    optimizer = tf.train.RMSPropOptimizer(learning_rate=0.05)
+    loss_fn = tf.losses.log_loss
+    model.compile(optimizer, loss_fn)
+
+    sequences, labels = make_data()
+
+    model.fit(sequences, labels, epochs=3)
+
+    if not os.path.exists(args.model_dir):
+        os.makedirs(args.model_dir)
+    model.save(os.path.join(args.model_dir, 'model.hd5'))
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument(
+        '--model-dir',
+        type=str,
+        default='/tmp/tpu-template'
+    )
+    parser.add_argument(
+        '--use-tpu',
+        action='store_true'
+    )
+    parser.add_argument(
+        '--tpu',
+        default=None
+    )
+
+    args, _ = parser.parse_known_args()
+
+    main(args)
