2c36c409b64e18829707e228db0226e0acec93e9
Gonzalo Gasca Meza <gogasca@google.com>
2018-10-31 00:23:35 -0700

    Update setup.py

--- boston/keras/setup.py
+++ boston/keras/setup.py
@@ -2,9 +2,6 @@
 from setuptools import find_packages
 from setuptools import setup
 
-with open('requirements.txt') as f:
-  requirements = [l.strip('\n') for l in f if
-                  l.strip('\n') and not l.startswith('#')]
 
 setup(
   name='boston',
