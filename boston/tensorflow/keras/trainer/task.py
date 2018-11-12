ebb53d3630e31d0b834aca326d89a8c567181fe8
Gonzalo Gasca Meza <gogasca@google.com>
2018-11-12 15:07:11 -0800

    Clean up
    1. Add sample.sh script to run example automatically and for future testing.
    2. Remove setup_logging function as is creating double logging information in CMLE output.
    3. Re-organize files for better structure

--- boston/tensorflow/keras/trainer/task.py
+++ boston/tensorflow/keras/trainer/task.py
@@ -66,29 +66,12 @@ def get_args():
 		type=float,
 		default=.001,
 		help='learning rate for gradient descent, default=.001')
+	parser.add_argument(
+		'--verbosity',
+		choices=['DEBUG', 'ERROR', 'FATAL', 'INFO', 'WARN'],
+		default='INFO')
 
 	return parser.parse_args()
-
-
-def _setup_logging():
-	"""Sets up logging."""
-	logger = logging.getLogger()
-	logger.setLevel(logging.INFO)
-	# Set tf logging to avoid duplicate logging. If the handlers are not removed,
-	# then we will have duplicate logging :
-	# From tf loggging written to stderr stream, and
-	# From python logger written to stdout stream.
-	tf_logger = logging.getLogger('tensorflow')
-	while tf_logger.handlers:
-		tf_logger.removeHandler(tf_logger.handlers[0])
-
-	# Redirect INFO logs to stdout
-	stdout_handler = logging.StreamHandler(sys.stdout)
-	stdout_handler.setLevel(logging.INFO)
-	logger.addHandler(stdout_handler)
-
-	# Suppress C++ level warnings.
-	os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 
 
 def train_and_evaluate(hparams):
@@ -156,7 +139,7 @@ def train_and_evaluate(hparams):
 if __name__ == '__main__':
 
 	args = get_args()
-	_setup_logging()
+	tf.logging.set_verbosity(args.verbosity)
 
 	# Run the training job
 	hparams = hparam.HParams(**args.__dict__)
--- imdb/tensorflow/keras/trainer/task.py
+++ imdb/tensorflow/keras/trainer/task.py
@@ -66,28 +66,12 @@ def get_args():
 		default=.001,
 		type=float,
 		help='Learning rate for gradient descent, default=.001')
+	parser.add_argument(
+		'--verbosity',
+		choices=['DEBUG', 'ERROR', 'FATAL', 'INFO', 'WARN'],
+		default='INFO')
+	
 	return parser.parse_args()
-
-
-def _setup_logging():
-	"""Sets up logging."""
-	logger = logging.getLogger()
-	logger.setLevel(logging.INFO)
-	# Set tf logging to avoid duplicate logging. If the handlers are not removed,
-	# then we will have duplicate logging :
-	# From tf loggging written to stderr stream, and
-	# From python logger written to stdout stream.
-	tf_logger = logging.getLogger('tensorflow')
-	while tf_logger.handlers:
-		tf_logger.removeHandler(tf_logger.handlers[0])
-
-	# Redirect INFO logs to stdout
-	stdout_handler = logging.StreamHandler(sys.stdout)
-	stdout_handler.setLevel(logging.INFO)
-	logger.addHandler(stdout_handler)
-
-	# Suppress C++ level warnings.
-	os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 
 
 def train_and_evaluate(hparams):
@@ -142,9 +126,9 @@ def train_and_evaluate(hparams):
 
 
 if __name__ == '__main__':
+	
 	args = get_args()
-	# Set Python level verbosity.
-	_setup_logging()
+	tf.logging.set_verbosity(args.verbosity)
+	
 	hparams = hparam.HParams(**args.__dict__)
-
 	train_and_evaluate(hparams)
