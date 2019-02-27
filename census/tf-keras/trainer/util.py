336f6c29a082ca01cf5ec7cc7371620e583b8640
Alec Glassford <alecglassford@google.com>
2019-02-27 12:34:31 -0800

    Update license header

--- census/tf-keras/trainer/util.py
+++ census/tf-keras/trainer/util.py
@@ -1,13 +1,16 @@
-# Copyright 2018 Google Inc. All Rights Reserved. Licensed under the Apache
-# License, Version 2.0 (the "License"); you may not use this file except in
-# compliance with the License. You may obtain a copy of the License at
-# http://www.apache.org/licenses/LICENSE-2.0
-
+# Copyright 2019 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
 # Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
-# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
-# License for the specific language governing permissions and limitations under
-# the License.
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 """Utilities to download and preprocess the Census data.
 """
 from __future__ import absolute_import
@@ -131,7 +134,7 @@ def download(data_dir):
   if not tf.gfile.Exists(eval_file_path):
     _download_and_clean_file(eval_file_path, EVAL_URL)
   
-  return training_file_path, eval_file_path  
+  return training_file_path, eval_file_path
 
 
 def preprocess(dataframe):
