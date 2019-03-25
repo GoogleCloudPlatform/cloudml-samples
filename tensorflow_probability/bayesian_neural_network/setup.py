8179e2d4a9f5ab5cd56fcb294b2ee51448c923eb
Yu-Han Liu <yuhanliu@google.com>
2019-03-25 13:18:33 -0700

    add bnn sample
    Change-Id: I0a388175b55f63c3b91938f0894388381d019eeb

---
+++ tensorflow_probability/bayesian_neural_network/setup.py
@@ -0,0 +1,27 @@
+# Copyright 2019 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+import setuptools
+
+REQUIRES = [
+    'tensorflow_probability==0.5',
+    'seaborn==0.9.0'
+]
+
+setuptools.setup(name='tfp_example_bayesian_neural_network',
+      version='0.0.1',
+      install_requires=REQUIRES,
+      packages=setuptools.find_packages()
+)
