d9cec34a5f05ee228f1e2185e73f93ae2fea0b94
Shixin Luo <luoshixin@google.com>
2019-04-09 16:52:34 +0800

    add predict.sh for online prediction

--- sklearn/sklearn-template/template/scripts/deploy.sh
+++ sklearn/sklearn-template/template/scripts/deploy.sh
@@ -14,7 +14,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-# Convenience script for running ML training jobs.
+# Convenience script for deploying trained scikit-learn model.
 #
 # Prerequisites:
 #   - Google Cloud SDK
