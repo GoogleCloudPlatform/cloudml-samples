b38958728476f798efdf1c8271f8f251151ad950
Shixin Luo <luoshixin@google.com>
2019-04-10 10:21:17 +0800

    modify according review comments

--- sklearn/sklearn-template/template/scripts/deploy.sh
+++ sklearn/sklearn-template/template/scripts/deploy.sh
@@ -27,18 +27,18 @@
 #   $2: Name of the model to be deployed
 #   $3: Version of the model to be deployed
 
-MODEL_BINARIES=$1
+MODEL_DIR=$1
 MODEL_NAME=$2
 VERSION_NAME=$3
 
 REGION=us-central1
 FRAMEWORK=SCIKIT_LEARN
 RUN_TIME=1.13
-PYTHON_VERSION=3.5 # only support python 2.7 and 3.5
+PYTHON_VERSION=2.7 # only support python 2.7 and 3.5
 
 gcloud ml-engine models list | grep $MODEL_NAME &> /dev/null
 if [ $? == 0 ]; then
-   echo "Model already existed"
+   echo "Model already exists."
 else
     # 1. Create model
     gcloud ml-engine models create $MODEL_NAME \
@@ -48,12 +48,12 @@ fi
 
 gcloud ml-engine versions list --model=$MODEL_NAME | grep $VERSION_NAME &> /dev/null
 if [ $? == 0 ]; then
-   echo "Version already existed"
+   echo "Version already exists."
 else
     # 2. Create version
     gcloud ml-engine versions create $VERSION_NAME \
     --model $MODEL_NAME \
-    --origin $MODEL_BINARIES \
+    --origin $MODEL_DIR \
     --framework $FRAMEWORK \
     --runtime-version=$RUN_TIME \
     --python-version=$PYTHON_VERSION
